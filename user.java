05bd2d015583042042ec90cb3201feaf301619dc
dhwanil-ditani <ditanidhwanil@gmail.com>
2019-11-01 16:01:18 +0530

    added Main class and methods for user class

---
+++ user.java
@@ -0,0 +1,29 @@
+class User {
+    private String mail_id, ph_no, addr;
+
+    public String getMail_id() {
+        return mail_id;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getPh_no() {
+        return ph_no;
+    }
+
+    public void setPh_no(String ph_no) {
+        this.ph_no = ph_no;
+    }
+
+    public void setMail_id(String mail_id) {
+        this.mail_id = mail_id;
+    }
+
+    
+}

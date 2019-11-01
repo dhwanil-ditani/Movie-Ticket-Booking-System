153139e6575bfe081401db44dd1a62654cc22169
dhwanil-ditani <ditanidhwanil@gmail.com>
2019-11-01 17:18:50 +0530

    added Layout class

---
+++ Layout.java
@@ -0,0 +1,39 @@
+class Layout {
+
+    public static void exit() throws InterruptedException {
+        clearScreen();
+        System.out.println("Exiting...............");
+        processBar();
+        clearScreen();
+    }
+
+    public static void start() throws InterruptedException {
+        clearScreen();
+        System.out.println("Estelle");
+        System.out.println("Loading:");
+        processBar();
+        clearScreen();
+    }
+
+    public static void processBar() throws InterruptedException {
+        for(int i = 0; i<50; i++)
+        {
+            System.out.print("[");
+            for(int j = 0; j<50; j++)
+            {
+                if( j <= i)
+                    System.out.print("*");
+                else
+                    System.out.print("-");
+            }
+            System.out.print("]");
+            Thread.sleep(50);
+            System.out.print("\r");
+        }
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}
--- Main.java
+++ Main.java
@@ -2,20 +2,7 @@ import java.io.File;
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) throws Exception {
-        
+
     }
 
 }
-
-/*
-[Now Showing]
-Joker Housefull4 
-
-
-[Timings]
-12:00pm 2:00pm 5:00pm
-
-
-
-
-*/

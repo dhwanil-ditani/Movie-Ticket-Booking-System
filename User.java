8a0df1002c84d326d92351e3574c4f63b5849a4d
dhwanil-ditani <ditanidhwanil@gmail.com>
2019-11-03 13:11:14 +0530

    added signin method

--- User.java
+++ User.java
@@ -1,9 +1,12 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.io.FileWriter;
 
 public class User {
-    private String mail_id, ph_no, username, password;
+    private String mail_id, username, password;
+    private long ph_no;
 
     public final Scanner scan = new Scanner(System.in);
 
@@ -11,11 +14,11 @@ public class User {
         return mail_id;
     }
 
-    public String getPh_no() {
+    public long getPh_no() {
         return ph_no;
     }
 
-    public void setPh_no(String ph_no) {
+    public void setPh_no(long ph_no) {
         this.ph_no = ph_no;
     }
 
@@ -55,12 +58,16 @@ public class User {
                 switch(temp[0]) {
                     case "Username":
                         setUsername(temp[1]);
+                        break;
                     case "PhoneNo":
-                        setPh_no(temp[1]);
+                        setPh_no(Long.parseLong(temp[1]));
+                        break;
                     case "MailId":
                         setMail_id(temp[1]);
+                        break;
                     case "Password":
                         setPassword(temp[1]);
+                        break;
                 }
             }
         }while(!getUsername().equals(username) || !getPassword().equals(password));
@@ -72,7 +79,7 @@ public class User {
             this.username = null;
             this.mail_id = null;
             this.password = null;
-            this.ph_no = null;
+            this.ph_no = 0;
             return false;
         }
     }
@@ -97,12 +104,70 @@ public class User {
                 return login();
             }
             else {
-                return false;
+                System.out.print("SignUp Instead?[y/n]");
+                choice = scan.nextLine().charAt(0);
+                if(choice == 'y') {
+                    return signUp();
+                }
+                else {
+                    return false;
+                }
             }
         }
     }
 
-    public void signUp() {
+    public boolean signUp() throws IOException {
+		Layout.clearScreen();
+		Layout.displayEstelle();
+		
+		File f = new File("./Accounts.txt");
+		
+		
+		FileInput in = new FileInput(f);
+		String s = "";
+		char c;
+        while(in.ready()) {
+            c = (char)in.read();
+            s = s.concat(String.valueOf(c));
+        }
+		in.close();
+		
+        String[] str = s.trim().split("\n");
+        String username;
+		System.out.println("\t\tSignUp\t\t");
+        do {
+            System.out.print("Enter your username: ");
+            username = scan.nextLine();
+            for(int i=0; i<str.length; i++) {
+                if(str[i].split(" ")[0].equals("Username:".concat(username))) {
+                    System.out.println("Username already exists.");
+                    username = null;
+                    break;
+                }
+            }
+            setUsername(username);
+        }while(this.username == null);
+        int error;
+        do {
+            System.out.print("Enter your PhoneNo: ");
+            error = 0;
+            try {
+                setPh_no(Long.parseLong(scan.nextLine()));
+            }
+            catch(NumberFormatException e) {
+                error = 1;
+            }
+        }while(error == 1);
 
+        System.out.print("Enter your MailId: ");
+        setMail_id(scan.nextLine());
+        System.out.print("Enter your Password: ");
+        setPassword(scan.nextLine());
+
+        FileWriter writer = new FileWriter(f);
+		writer.write(s + "\n");
+        writer.append("Username:"+this.username+" "+"PhoneNo:"+this.ph_no+" "+"MailId:"+this.mail_id+" "+"Password:"+this.password);
+        writer.close();
+        return true;
     }
 }
